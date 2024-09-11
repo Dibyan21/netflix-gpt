aa0cdf13573658c08f06cd1988e9c32667a084e3
Dibyan Samantray <dibyansamantray@gmail.com>
2024-09-11 12:53:01 +0530

    Built Header and Login component with sign in/up form

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+// const Header = () => {
+//   return (
+//     <>
+//       <div className="flex px-[3rem] py-[1.5rem] mx-96 justify-between">
+//         <div>
+//           <img
+//             className="w-[12rem] h-[5.5rem]"
+//             src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+//             alt="logo"
+//           />
+//         </div>
+//         <div></div>
+//       </div>
+//     </>
+//   );
+// };
+
+const Header = () => {
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+      <img className="w-44"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
