1936a51d1bc20d18859cd6e8bbfada78c779a731
Dibyan Samantray <dibyansamantray@gmail.com>
2024-09-28 17:18:37 +0530

    Initial setup of Gemini API to give movie suggestions

--- src/components/GPTSearchBar.jsx
+++ src/components/GPTSearchBar.jsx
@@ -1,18 +1,37 @@
 import { useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
+import { useRef } from "react";
+import model from "../utils/gemini";
 
 const GPTSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
+  const searchText = useRef(null);
+
+  const handleGPTSearchClick = async () => {
+    //Make an API call to Gemini API and get movie suggestions
+    const prompt =
+      "Act as a Movie Recommendation system and suggest some movies for the query : " +
+      searchText.current.value +
+      ". Only give me names of 5 movies, comma separated like the example result given ahead. Example Result: Gaddar, Sholay, Don, Golmaal, Koi Mil Gaya";
+
+    const gptResults = await model.generateContent(prompt);
+    console.log(gptResults.response.text());
+  };
 
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className="w-1/2 bg-black grid grid-cols-12">
+      <form
+        className="w-1/2 bg-black grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}>
         <input
+          ref={searchText}
           type="text"
           className="p-4 m-4 col-span-9"
           placeholder={lang[langKey].gptSearchPlaceholder} //since we don't have anything as langKey in the actual object 'lang' in store we use lang[langKey]
         />
-        <button className="col-span-3 m-4 py-2 px-4 bg-red-600 text-white rounded-lg">
+        <button
+          className="col-span-3 m-4 py-2 px-4 bg-red-600 text-white rounded-lg"
+          onClick={handleGPTSearchClick}>
           {lang[langKey].search}
         </button>
       </form>
---
+++ src/utils/gemini.js
@@ -0,0 +1,35 @@
+import { GEMINI_KEY } from "./constants";
+import {
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
+} from "@google/generative-ai";
+
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+];
+
+const genAI = new GoogleGenerativeAI(GEMINI_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  safetySettings: safetySettings,
+});
+
+export default model;
+
+// const prompt = "Write a story about a magic backpack.";
